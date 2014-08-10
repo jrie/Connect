a8c204ee6c250d13bb48c33a3d3a3632fc01f4b6
jrie <janpeterr@freenet.de>
2014-08-10 19:58:23 +0200

    Start of file seperation for ui handling and css
    CSS is now seperated for the main UI as well as UI handling javascript.
    HTML cleaned up, added title.

---
+++ connect_ui.js
@@ -0,0 +1,33 @@
+function checkMouseResize(evt) {
+    if (evt.clientX <= 250 || evt.clientX >= (document.getElementById('gameScreen').clientWidth - 80)) {
+        return;
+    }
+    document.getElementById('gameScreenArea').width = evt.clientX - document.getElementById('gameScreenArea').offsetLeft;
+}
+
+function scaleCombatScreen() {
+    document.getElementById('combatArea').width = document.getElementById('combatDisplay').clientWidth * 0.96;
+    document.getElementById('combatArea').height = document.getElementById('combatDisplay').clientHeight * 0.75;
+    document.getElementById('combatReport').style.height = document.getElementById('combatDisplay').clientHeight * 0.18;
+    //console.log(document.getElementById('combatDisplay').clientHeight);
+}
+
+
+// UI element handlers
+document.getElementById('resizeRight').addEventListener('mousedown', function(evt) {
+    window.addEventListener('mousemove', checkMouseResize);
+});
+
+window.addEventListener('mouseup', function() {
+    window.removeEventListener('mousemove', checkMouseResize);
+});
+
+
+window.addEventListener('resize', function() {
+    scaleCombatScreen();
+});
+
+
+window.addEventListener('load', function() {
+    scaleCombatScreen();
+});
